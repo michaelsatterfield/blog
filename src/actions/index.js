b1313397609f60539b7fb8e639a0ec49defb2263
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-06-04 11:57:02 -0500

    creating reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 //action creator with thunk middleware
+// asynchronous api action creator!!
 export const fetchPost = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts')
 
