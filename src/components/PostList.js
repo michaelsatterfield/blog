df11141821d5dc11800a444357f0ef4c5ffea177
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-06-04 17:52:55 -0500

    getting api info

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,19 +1,26 @@
-import React, {Component} from 'react';
-import { connect} from "react-redux";
+import React from 'react';
+import { connect } from "react-redux";
 import { fetchPost } from "../actions";
 
-class PostList extends Component {
+class PostList extends React.Component {
     componentDidMount() {
         this.props.fetchPost()
     }
 
     render() {
+        console.log(this.props.posts )
         return (
             <div>
                 Post List
             </div>
         );
     }
+
+
 }
 
-export default connect(null, {fetchPost})(PostList);
+const mapStateToProps = (state) => {
+ return { posts: state.posts }
+}
+
+export default connect(mapStateToProps, {fetchPost})(PostList);
--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,5 +1,16 @@
-export default () => {
+
+
+
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (state = [], action ) => {
     //only use whats int the reducer like args and computations on those args etc
-    return 1
-}
+ switch (action.type){
+     case 'FETCH_POSTS':
+         return action.payload;
+     default:
+         return state;
+ }
+
+};
 
