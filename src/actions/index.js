a2d94b4467688ce4815a992c832d865088d4c732
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-06-04 11:37:01 -0500

    wiring up thunk to redux store

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,13 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPost = () =>{
-   const promise =  jsonPlaceholder.get('/posts')
+//action creator
+export const fetchPost = () => {
+    return function (dispatch, getState) {
+        const promise = jsonPlaceholder.get('/posts')
 
-    return {
-        type: 'FETCH_POSTS',
-        payload: promise
+        return {
+            type: 'FETCH_POSTS',
+            payload: promise
+        }
     };
 };
