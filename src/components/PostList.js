bcd9788cff807468206896282f955282118c2d6f
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-06-09 16:11:28 -0500

    rendering api data return

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -6,6 +6,15 @@ class PostList extends React.Component {
     componentDidMount() {
         this.props.fetchPost()
     }
+     renderList() {
+        return this.props.posts.map( post => {
+            return (
+                <div className="item" key={post.id}>
+
+                </div>
+            )
+        })
+     }
 
     render() {
         console.log(this.props.posts )
