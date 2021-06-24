01d21f60b4fd9c705765b2dd12d0e14ad0378be9
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-06-24 18:41:54 -0500

    onetime memoization

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 //action creator with thunk middleware
@@ -10,8 +11,17 @@ export const fetchPost = () => async dispatch => {
 
 
 //action creator api to return users by id
-export const fetchUser = (id) => async dispatch => {
-    const response = await jsonPlaceholder.get(`/users/${id}`);
+// export const fetchUser = (id) => async dispatch => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+//
+//     dispatch({type: 'FETCH_USER', payload: response.data})
+// };
 
-    dispatch({type: 'FETCH_USER', payload: response.data})
+
+export const fetchUser = function (id) {
+    return async function (dispatch) {
+        const response = await jsonPlaceholder.get(`/users/${id}`);
+
+        dispatch({type: 'FETCH_USER', payload: response.data})
+    }
 };
